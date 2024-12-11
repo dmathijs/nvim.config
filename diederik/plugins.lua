998690534c0d3d4e4f1ed4ef51c21e03eb7763e6
dmathijs <diederik.mathijs@gmail.com>
2024-12-11 15:42:23 +0100

    Set theme to rose-pine moon

--- diederik/plugins.lua
+++ diederik/plugins.lua
@@ -2,5 +2,9 @@ return {
   "folke/neodev.nvim",
   "folke/which-key.nvim",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "nvim-telescope/telescope.nvim"
+  "nvim-telescope/telescope.nvim",
+  { "rose-pine/neovim", name = "rose-pine", config = function()
+    require("rose-pine").setup({ variant = "moon" })
+    vim.cmd("colorscheme rose-pine")
+  end }
 }
