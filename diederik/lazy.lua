7470ce183d01e3baefaa736c567759515e5fb090
Diederik Mathijs <diederik.mathijs@gmail.com>
2024-12-11 21:46:50 +0100

    small change

--- diederik/lazy.lua
+++ diederik/lazy.lua
@@ -57,10 +57,10 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
     vim.keymap.set('n', 'gd', '<cmd>lua require(\'omnisharp_extended\').lsp_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua require(\'omnisharp_extended\').lsp_references()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua require(\'omnisharp_extended\').lsp_implementation()<cr>', opts)
     vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
     vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
     vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
     vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
     vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
